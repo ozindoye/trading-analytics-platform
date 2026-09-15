f5a6ed23f6635dbec370c90174e31a53da0f5a3c
ozindoye <ozindoye@gmail.com>
2026-09-15 15:30:47 +0100

    Allow deployed frontend origin in CORS

--- src/main/java/com/ousman/trading_analytics_platform/config/WebConfig.java
+++ src/main/java/com/ousman/trading_analytics_platform/config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "https://https://trading-analytics-platform-ten.vercel.app"
+                )
                 .allowedMethods("GET");
     }
 }
